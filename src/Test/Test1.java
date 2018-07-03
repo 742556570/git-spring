b678195628e6809762919d3485fd255e7daf76d9
742556570 <742556570@qq.com>
2018-07-03 10:43:53 +0800

    Signed-off-by: 742556570 <742556570@qq.com>

--- src/Test/Test1.java
+++ src/Test/Test1.java
@@ -1,11 +1,11 @@
-package Test;
+﻿package Test;
 
 /**
  * Created by 宁负流年不负卿 on 2018/7/3.
  */
 public class Test1 {
     public static void main(String[] args) {
-
+            System.out.print("666666666");
     }
 
 }
